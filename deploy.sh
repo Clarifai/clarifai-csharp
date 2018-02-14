96e376d58280d631d839d9ea9dfd28b9953ca954
Rok Povšič <rok.povsic@gmail.com>
2018-02-14 18:07:24 +0100

    Removed deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-set -e
-
-rm -rf Clarifai/bin/Release
-$HOME/.dotnet/dotnet restore
-$HOME/.dotnet/dotnet pack -c Release
-$HOME/.dotnet/dotnet nuget push Clarifai/bin/Release/Clarifai.*.nupkg -k $NUGET_API_KEY -s https://www.nuget.org/api/v2/package
